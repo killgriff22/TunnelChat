9716d8c0c3183587765686dc5dbfb87e7125a3e9
killgriff22 <kilgrif22033@gmail.com>
2024-05-15 08:04:50 -0400

    MAYBE

--- CLIENT/classes.py
+++ CLIENT/classes.py
@@ -15,20 +15,23 @@ class tunnel:
         if self.tunnel.poll() is not None:
             self.spawn()
         try:
-            r = requests.get(f"http://localhost:{port}/")
+            r = self.post(f"http://localhost:{port}/")
             if r.status_code == 200:
                 return True
             return False
         except requests.exceptions.ConnectionError:
             return False
     def get_messages(self):
-        text = requests.post(f"http://localhost:{port}/messages",data={"user":self.user}).text
+        text = self.post(f"http://localhost:{port}/messages",data={"user":self.user}).text
         print(text)
         return eval(text)
     def send_message(self,message):
-        requests.post(f"http://localhost:{port}/send",data={"message":message,"user":self.user})
+        self.post(f"http://localhost:{port}/send",data={"message":message,"user":self.user})
     def register_user(self):
-        requests.post(f"http://localhost:{port}/register",data={"user":self.user})
+        self.post(f"http://localhost:{port}/register",data={"user":self.user})
     def logout(self):
-        requests.post(f"http://localhost:{port}/logout",data={"user":self.user})
+        self.post(f"http://localhost:{port}/logout",data={"user":self.user})
         self.kill()
+
+    def post(self,url,data):
+        return requests.post(url,json=data)
